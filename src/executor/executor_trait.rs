90c4425afe83ff6a53633413afbc8e49eda2ac78
daivy <2259492701@qq.com>
2026-05-20 15:17:24 +0800

    refactor(m5): add async_trait to Executor for consistency

--- src/executor/executor_trait.rs
+++ src/executor/executor_trait.rs
@@ -1,9 +1,12 @@
 //! Executor trait - async iterator interface
 
+use async_trait::async_trait;
+
 use crate::executor::ExecResult;
 use crate::storage::Result;
 
 /// Executor trait - 异步迭代器接口
+#[async_trait]
 pub trait Executor {
     /// 执行一次迭代，返回结果
     /// None 表示迭代结束（无更多结果）
